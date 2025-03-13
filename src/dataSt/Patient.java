e636bc26a06af9bef74516e0a111fad4645e4830
lindacelmaa <lindacelma98@gmail.com>
2025-03-13 11:13:07 +0200

    Patient with bugs

--- src/dataSt/Patient.java
+++ src/dataSt/Patient.java
@@ -1,5 +1,64 @@
 package dataSt;
 
 public class Patient {
-
+	private String name;
+	private String surname;
+	private int priority;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+	public Patient() {
+		setName("Test");
+		setSurname("Pacients");
+		setPriority(3);
+	}
+	
+	public Patient(String name, String surname, int priority) {
+		setName(name);
+		setSurname(surname);
+		setPriority(priority);
+	}
+	
+	public String toString() {
+		return name + " " + surname + ", " + priority;
+	}
+	
+	public int compareTo(Patient arg0) {
+		if(priority > arg0.priority)
+		{
+			return 1;
+		}
+		else if (priority < arg0.priority)
+		{
+			return -1;
+		}
+		else //abi ir vienādi
+		{
+			return 0;
+		}
+	}
+	
+	
 }
--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,6 +1,7 @@
 package service;
 
 import dataSt.MyHeap;
+import dataSt.Patient;
 
 public class MainService {
 	public static void main(String[] args) {
@@ -30,6 +31,34 @@ public class MainService {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		System.out.println("---------- Pacienti ----------------");
+		MyHeap<Patient> heapForPatients = new MyHeap<Patient>(2);
+		heapForPatients.enqueue(new Patient("Janis", "Be", 5));
+		heapForPatients.enqueue(new Patient("Anna", "Be", 6));
+		heapForPatients.enqueue(new Patient("Janisdd", "Be", 10));
+		heapForPatients.enqueue(new Patient("Janisdd", "Be", 8));
+		heapForPatients.enqueue(new Patient("Janidsas", "Be", 9));
+		
+		try {
+			heapForPatients.print();
+			System.out.println(heapForPatients.dequeue());
+			System.out.println(heapForPatients.dequeue());
+			
+			heapForPatients.makeEmpty();
+			heapForPatients.enqueue(new Patient("Kaspars", "Gudrais", 4));
+			heapForPatients.enqueue(new Patient("Ilze", "Gudrais", 3));
+			heapForPatients.print();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 
 	}
+	
+	
+	
 }
